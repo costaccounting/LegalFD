dedc99ad9fd2849f6eefae8ef77ec4e19f9f6795
shahriya07 <43189816+shahriya07@users.noreply.github.com>
2019-10-22 11:33:26 -0400

    new bean class

--- src/main/java/ca/sheridancollege/beans/RegistrationChildren.java
+++ src/main/java/ca/sheridancollege/beans/RegistrationChildren.java
@@ -1,5 +1,20 @@
 package ca.sheridancollege.beans;
 
+import javax.persistence.Entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+
 public class RegistrationChildren {
+	
+	private String childName1;
+	private String childName2;
+	private String childName3;
+	private String childName4;
+	
 
 }
--- src/main/java/ca/sheridancollege/beans/RegistrationMartialInfo.java
+++ src/main/java/ca/sheridancollege/beans/RegistrationMartialInfo.java
@@ -1,5 +1,32 @@
 package ca.sheridancollege.beans;
 
-public class RegistrationMartialInfo {
+import java.util.Date;
 
+import javax.persistence.Entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class RegistrationMartialInfo {
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private Date marriageDate;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private Date cohabitationDate;
+	private String marriagePlace;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private Date separationDate;
+	private String courtCase;
+	private String writtenAgreement;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private Date relevantDate;
+	private String safetyConcerns;
+	private String spouseCriminalRecord;
 }
