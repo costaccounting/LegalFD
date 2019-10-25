2ff447df2913fef15c1f8efbfb1a92ab0110e2bb
paras1103patel <paras1103patel@gmail.com>
2019-10-25 19:11:11 -0400

    Lawyer Document Edit

--- src/main/java/ca/sheridancollege/beans/LawyerDocEdit.java
+++ src/main/java/ca/sheridancollege/beans/LawyerDocEdit.java
@@ -22,6 +22,6 @@ public class LawyerDocEdit {
 	
 	private String docType;
 	private String formType;
-	private String price;
-	private boolean sale;
+	private double price;
+	private String sale;
 }
